9b369897a48089f0adb8d9513f030e2773b94b82
Valentin JOUFFRAY <valentin.jouffray@gmail.com>
2024-11-22 12:05:52 +0100

    tp1 done

--- src/test/java/java17/ex01/Method_01_Test.java
+++ src/test/java/java17/ex01/Method_01_Test.java
@@ -19,6 +19,9 @@ public class Method_01_Test {
 
         // TODO créer une méthode int sumAge()
         // TODO Cette méthode retourne le résultat de l'addition des ages des personnes
+        default int sumAge() {
+            return findAll().stream().mapToInt(Person::getAge).sum();
+        }
     }
     // end::IDao[]
 
@@ -48,7 +51,7 @@ public class Method_01_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoA.sumAge();
 
         assert result == 210;
     }
@@ -59,7 +62,7 @@ public class Method_01_Test {
         DaoB daoB = new DaoB();
 
         // TODO invoquer la méthode sumAge pour que le test soit passant
-        int result = 0;
+        int result = daoB.sumAge();
 
         assert result == 5050;
 
--- src/test/java/java17/ex02/Method_02_Test.java
+++ src/test/java/java17/ex02/Method_02_Test.java
@@ -1,11 +1,10 @@
 package java17.ex02;
 
-import java.util.List;
-
-import org.junit.Test;
-
 import java17.data.Data;
 import java17.data.Person;
+import org.junit.Test;
+
+import java.util.List;
 
 /**
  * Exercice 02 - Redéfinition
@@ -19,6 +18,9 @@ public class Method_02_Test {
         // TODO créer une méthode String format()
         // TODO la méthode retourne une chaîne de la forme [<nb_personnes> persons]
         // TODO exemple de résultat : "[14 persons]", "[30 persons]"
+        default String format() {
+            return "[" + findAll().size() + " persons]";
+        }
     }
     // end::IDao[]
 
@@ -36,6 +38,9 @@ public class Method_02_Test {
         // TODO la méthode retourne une chaîne de la forme DaoA[<nb_personnes> persons]
         // TODO exemple de résultat : "DaoA[14 persons]", "DaoA[30 persons]"
         // TODO l'implémentation réutilise la méthode format() de l'interface
+        public String format() {
+            return "DaoA" + IDao.super.format();
+        }
 
     }
     // end::DaoA[]
@@ -46,7 +51,7 @@ public class Method_02_Test {
         DaoA daoA = new DaoA();
 
         // TODO invoquer la méthode format() pour que le test soit passant
-        String result = null;
+        String result = daoA.format();
 
         assert "DaoA[20 persons]".equals(result);
     }
